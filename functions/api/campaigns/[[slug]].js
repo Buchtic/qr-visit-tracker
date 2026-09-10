220009946984fb9d3f38de71f578a656f3d7777d
Buchtič <martin.buchta@gmail.com>
2026-09-10 13:00:33 +0200

    review

--- functions/api/campaigns/[[slug]].js
+++ functions/api/campaigns/[[slug]].js
@@ -204,7 +204,7 @@ export async function onRequestOptions() {
     return new Response(null, {
         headers: {
             "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+            "Access-Control-Allow-Methods": "GET, POST, DELETE, OPTIONS",
             "Access-Control-Allow-Headers": "Content-Type, X-Admin-Token"
         }
     });
